9304184385bbf4d71cfb399eb51f9e2fefd1e1dd
Bob Jones <wertzuio987@web.de>
2025-03-13 16:44:04 +0100

    Add styling

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,10 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
+    <link
+      rel='stylesheet'
+      href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
+    />
     <App />
   </React.StrictMode>
 );
